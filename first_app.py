4b330a8b188aa21c3835ea8cb2a3b817c9eb1836
eoghanleo1 <108322346+eoghanleo1@users.noreply.github.com>
2022-06-28 16:34:31 +1000

    Update first_app.py

--- first_app.py
+++ first_app.py
@@ -3,6 +3,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & blueberry Oatmeal')
-streamlit.text('Kale, Spinach, Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & blueberry Oatmeal 🥣')
+streamlit.text('🥗 Kale, Spinach, Rocket Smoothie 🥗')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg 🐔')
+streamlit.text('🥑Avo Toast Bitch 🥑')
+streamlit.header('build your won fruit smoothie')
